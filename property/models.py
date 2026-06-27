31e7452b7e62a0409c97c9b55e10547d3269679c
Ангелина Петрова <linalleks@gmail.com>
2026-06-27 19:57:15 +0700

    remove owners fields from Flats

--- property/models.py
+++ property/models.py
@@ -5,10 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
-                                        max_length=20, blank=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField('Новостройка',
                                        choices=[(None, 'Неизвестно'), (True, 'Да'), (False, 'Нет')],
                                        null=True,
