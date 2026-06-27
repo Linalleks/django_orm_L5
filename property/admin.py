3df227bebb67ee1bda2d4a2b9379868bd27d35dc
Ангелина Петрова <linalleks@gmail.com>
2026-06-27 18:36:50 +0700

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -6,7 +6,8 @@ from .models import Complaint, Flat
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address')
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year']
+    list_display = ['address', 'price', 'owners_phonenumber',
+                    'owner_pure_phone', 'new_building', 'construction_year']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['likes']
--- property/models.py
+++ property/models.py
@@ -6,7 +6,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', max_length=20, blank=True)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
+                                        max_length=20, blank=True)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField('Новостройка',
                                        choices=[(None, 'Неизвестно'), (True, 'Да'), (False, 'Нет')],
